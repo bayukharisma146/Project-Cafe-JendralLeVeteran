1fe408481eff6a0054dfb6346f2d39d3af366fac
Bayu Kharisma <kelvinpandu19@gmail.com>
2025-06-22 19:46:28 +0700

    fix: resolve next.config.js conflict

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-<<<<<<< HEAD
 const nextConfig = {
   // output: "export",
   // Tambahkan jika pakai image
@@ -7,8 +6,4 @@ const nextConfig = {
     unoptimized: true,
   },
 };
-=======
-const nextConfig = {};
->>>>>>> d9b9e6fd164146f209b9f394e9a83ef33bc34a0d
-
 export default nextConfig;
